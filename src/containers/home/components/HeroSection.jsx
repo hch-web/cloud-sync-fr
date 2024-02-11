d4575a18d3a3404da428f0eac536d1615fa83091
fizazahra <159478189+fizazahra@users.noreply.github.com>
2024-02-12 03:20:20 +0500

    Update HeroSection.jsx

--- src/containers/home/components/HeroSection.jsx
+++ src/containers/home/components/HeroSection.jsx
@@ -7,14 +7,19 @@ function HeroSection() {
   return (
     <Box sx={heroSectionStyles}>
       <Container fixed>
-        <Typography variant="h1" fontSize={80} mb={3} fontWeight={600}>
-          Heading
+        <Typography variant="h1" fontSize={50} mb={3} fontWeight={600}>
+          Where Connectivity Transcends Boundaries
         </Typography>
 
         <Typography variant="h6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores consequuntur molestiae officia.
-          Magni praesentium fugiat itaque ex, ea ipsum aperiam deleniti, est quam quasi id amet veritatis
-          maxime voluptatem corrupti.
+          Welcome to CloudSync, your premier destination for sophisticated cloud services
+          encompassing VBS, web hosting, and cloud storage solutions. At CloudSync, we are
+          committed to simplifying your digital endeavors by providing robust, tailored
+          solutions that epitomize reliability, scalability, and accessibility. Whether
+          you are an emergent startup or a seasoned enterprise, our platform facilitates
+          synchronized data, fosters enhanced collaboration, and streamlines operations
+          with unparalleled elegance. Embrace the convergence of innovation and reliability
+          with CloudSync as we prioritize your success.
         </Typography>
       </Container>
     </Box>
